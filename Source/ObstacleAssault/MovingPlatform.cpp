b7fa5316002b13f569f806d93ebd4d6a5ac038f8
NikolayDimitrov69 <4mi0800004@g.fmi.uni-sofia.bg>
2023-08-03 10:20:03 +0300

    Initial commit

---
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	startLocation = GetActorLocation();
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	MovePlatform(DeltaTime);
+
+	RotatePlatform(DeltaTime);
+}
+	
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{
+
+	if (shouldPlatformReturn())
+	{
+		FVector moveDirection = platformVelocity.GetSafeNormal();
+		startLocation = startLocation + moveDirection * moveDistance;
+		SetActorLocation(startLocation);
+		platformVelocity = -platformVelocity;
+	}
+	else
+	{
+		FVector currentLocation = GetActorLocation();
+		currentLocation += platformVelocity * DeltaTime;
+		SetActorLocation(currentLocation);
+	}
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
+	AddActorLocalRotation(rotation * DeltaTime);
+
+}
+
+bool AMovingPlatform::shouldPlatformReturn() const
+{
+	return getDistanceMoved() > moveDistance;
+}
+
+float AMovingPlatform::getDistanceMoved() const
+{
+	return FVector::Dist(startLocation, GetActorLocation());
+}
---
+++ Source/ObstacleAssault/MovingPlatform.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class OBSTACLEASSAULT_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+private:
+	UPROPERTY(EditAnywhere, Category="Movement")
+	FVector platformVelocity = FVector(100, 0, 0);
+	UPROPERTY(EditAnywhere, Category="Movement")
+	float moveDistance = 100.f;
+	UPROPERTY(EditAnywhere, Category="Rotation")
+	FRotator rotation;
+
+	FVector startLocation;
+
+	void MovePlatform(float DeltaTime);
+
+	void RotatePlatform(float DeltaTime);
+
+	bool shouldPlatformReturn() const;
+
+	float getDistanceMoved() const;
+};
